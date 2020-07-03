5793e5911488552a46aa1f5092758f22d395b6f7
Colt Wilkens <scavengerq@gmail.com>
2020-07-03 17:59:08 +0200

    github.io. slides-list textarea bound to slides.

--- src/app/slides/slides-list.component.ts
+++ src/app/slides/slides-list.component.ts
@@ -13,6 +13,28 @@ export class SlidesListComponent implements OnInit {
 
   constructor(private slideService: SlideService) { }
 
+  _slidesText: string;
+  get slidesText(): string {
+    return this.slideService.getSlides()
+      .map(value => value.slideText)
+      .join('\n');
+  }
+  set slidesText(value: string) {
+    // split and separate and set all slides
+    var textArray: string[] = value.split('\n');
+    var symbolArray: symbol[] = this.slideService.getSlides().map(slide => slide.id);
+
+    for (let index = 0; index < textArray.length; index++) {
+      var id;
+      if (index < symbolArray.length) {
+        id = symbolArray[index];
+      } else {
+        id = this.slideService.addSlide().id;
+      }
+      this.slideService.setSlideText(id, textArray[index]);
+    }
+  }
+
   ngOnInit() {
     this.updateSlides();
     // get current slide Subject from service
