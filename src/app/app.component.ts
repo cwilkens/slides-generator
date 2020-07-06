cd10c95c7305d36da3a66bb5e9fc4dab7f579d32
Colt Wilkens <scavengerq@gmail.com>
2020-07-06 18:08:22 +0200

    add slide export

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import pptxgen from "pptxgenjs";
+import { SlideService } from './slides/slide.service';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,32 @@ import pptxgen from "pptxgenjs";
 export class AppComponent {
   pageTitle = 'slides-generator';
 
-  onClick(){
-    var pptx = new pptxgen();
-    var slide = pptx.addSlide();
-    slide.addText('Hello World!', { x: 1.5, y: 1.5, fontSize: 18, color: '363636' });
-    pptx.writeFile('Sample Presentation');
+  constructor(private slideService: SlideService) { }
+
+  async onExport() {
+    const slides = this.slideService.getSlides();
+    const pptx = new pptxgen();
+
+    for (let index = 0; index < slides.length; index++) {
+      const slide = slides[index];
+      const pptSlide = pptx.addSlide();
+
+      const image = new Image();
+      image.src = slide.slideImage;
+      await image.decode();
+
+      pptSlide.addImage({
+        data:slide.slideImage,
+        x:0, y:0, w: image.naturalWidth/100, h: image.naturalHeight/100,
+        sizing: {type: 'cover', w: 10, h: 5.625}
+      })
+      pptSlide.addShape(pptx.ShapeType.rect, { x:0, y:'74.074%', w:'100%', h:'18.518%', fill:'4D4D4D' });
+      pptSlide.addShape(pptx.ShapeType.line, { x:0, y:'76.8518%', w:'100%', h:0, line:'838383', lineSize:1 });
+      pptSlide.addShape(pptx.ShapeType.line, { x:'9.89583%', y:'74.074%', w:0, h:'18.518%', line:'838383', lineSize:1 });
+
+      pptSlide.addText(slide.slideText, { x:'10.4583%', y:'75.3518%', w:'100%', h:'17.018%', fontFace: "Arial", fontSize: 36, color: "F0F0F0" });
+    }
+    
+    pptx.writeFile('Presentation');
   }
 }
