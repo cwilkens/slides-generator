a505908513b91b8a7684b3938d3bfe926452aa5d
cwilkens <scavengerq@gmail.com>
2020-07-17 14:38:32 +0000

    Deploying to heroku from  @ cc3200db0e9cb792bdfe23197ae80e5cbc523369 🚀

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,36 @@
 import * as http from "http";
+import * as https from "https";
+import * as httpProxy from "http-proxy";
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Server is working! Headers:' + '\n' + JSON.stringify(req.headers, null, 2));
-    res.end();
-}).listen(80);
+const HOST: string = process.env.HOST || '0.0.0.0';
+const PORT: number = +(process.env.PORT || 8080);
+const SSL: boolean = (process.env.SSL != undefined);
+
+function parseUrl() {
+
+}
+
+function getHandler(proxy: httpProxy): http.RequestListener {
+
+    return function(req, res) {
+        console.log(req.url);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write('Server is working! Headers:' + '\n' + JSON.stringify(req.headers, null, 2));
+        res.end();
+        return;
+    }
+}
+
+let proxy = httpProxy.createServer();
+let requestHandler = getHandler(proxy);
+
+let server: http.Server;
+if (SSL) {
+    server = https.createServer(requestHandler);
+} else {
+    server = http.createServer(requestHandler);
+}
+
+server.listen(PORT, HOST, () => {
+    console.log('Running server on '+HOST+':'+PORT);
+});
